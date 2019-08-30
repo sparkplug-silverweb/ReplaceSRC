6f6b2442fd49ae438134452b0eb82f8981abf120
Lester Serdenia <lserdenia@hotmail.com>
2019-08-30 16:15:08 +0800

    Add project files.

---
+++ ReplaceSRC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReplaceSRC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
